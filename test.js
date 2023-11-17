e01fa3a46314a3147fe5054a03f3a6c2bb432219
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-11-17 15:35:56 +0000

    update

--- test.js
+++
@@ -1,8 +0,0 @@
-const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
-        method: "GET",
-        headers: { Authorization: `Bearer ${token}` },
-    });
-    const data = await response.json();
-    dispatch(setPosts({ posts: data }));
-}
